d51e407393bcac7c240357f578fbccf1e133df12
Nithishvb <Nithishvb@users.noreply.github.com>
2025-02-25 01:33:47 +0530

    feat: implement swap API with send and swap transaction instructions

--- src/app/api/swap/route.ts
+++ src/app/api/swap/route.ts
@@ -1,33 +1,93 @@
 import { NextResponse } from "next/server";
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { createJupiterApiClient } from "@jup-ag/api";
+import {
+  getAssociatedTokenAddress,
+  createTransferInstruction,
+} from "@solana/spl-token";
 
 export async function POST(req: Request) {
   try {
-    const {
-      userPublicKey,
-      inputMint,
-      outputMint,
-      inAmount,
-      destinationTokenAccount,
-    } = await req.json();
+    const { userPublicKey, inputMint, outputMint, inAmount, merchantAddress } =
+      await req.json();
 
-    const response = await fetch("https://quote-api.jup.ag/v6/swap", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        userPublicKey,
-        wrapAndUnwrapSol: false,
-        inputMint,
-        outputMint,
-        inAmount,
-        slippageBps: 50,
-        destinationTokenAccount,
-      }),
+    const buyer = new PublicKey(userPublicKey);
+    const merchant = new PublicKey(merchantAddress);
+    const inputMintKey = new PublicKey(inputMint);
+    const outputMintKey = new PublicKey(outputMint);
+
+    const jupiterQuoteApi = createJupiterApiClient();
+
+    const quoteResponse = await jupiterQuoteApi.quoteGet({
+      inputMint: inputMintKey.toBase58(),
+      outputMint: outputMintKey.toBase58(),
+      amount: inAmount.toString(),
+      swapMode: "ExactIn",
     });
 
-    const data = await response.json();
+    if (!quoteResponse || !quoteResponse.routePlan.length) {
+      return NextResponse.json(
+        {
+          error: "No swap route found",
+        },
+        { status: 500 }
+      );
+    }
+
+    const buyerTokenAccount = await getAssociatedTokenAddress(
+      inputMintKey,
+      buyer
+    );
+    const merchantTokenAccount = await getAssociatedTokenAddress(
+      inputMintKey,
+      merchant
+    );
+
+    const transferInstruction = createTransferInstruction(
+      buyerTokenAccount,
+      merchantTokenAccount,
+      buyer,
+      inAmount
+    );
+
+    const { setupInstructions } = await jupiterQuoteApi.swapInstructionsPost({
+      swapRequest: {
+        userPublicKey: merchantAddress,
+        quoteResponse: quoteResponse,
+      },
+    });
+
+    const transactionInstructions: TransactionInstruction[] =
+      setupInstructions.map((instruction) => {
+        return new TransactionInstruction({
+          keys: instruction.accounts.map((account) => {
+            return {
+              pubkey: new PublicKey(account.pubkey),
+              isSigner: account.isSigner,
+              isWritable: account.isWritable,
+            };
+          }),
+          programId: new PublicKey(instruction.programId),
+          data: Buffer.from(instruction.data, "utf-8"),
+        });
+      });
+
+    const transaction = new Transaction().add(
+      transferInstruction,
+      ...transactionInstructions
+    );
+
+    const serializedTransaction = transaction
+      .serialize({ requireAllSignatures: false })
+      .toString("base64");
+
     return NextResponse.json(
       {
-        data,
+        transaction: serializedTransaction,
       },
       { status: 200 }
     );
